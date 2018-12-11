72127a704c00cb840c11efc62e3ce1d54253d862
Henrique de Castro <lhccorrea@hotmail.com>
2018-12-11 14:20:53 -0200

    listando as Categorias com DTO

--- src/main/java/com/henrique/aplicacao/services/CategoriaService.java
+++ src/main/java/com/henrique/aplicacao/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.henrique.aplicacao.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.henrique.aplicacao.domain.Categoria;
+import com.henrique.aplicacao.dto.CategoriaDTO;
 import com.henrique.aplicacao.repositories.CategoriaRepository;
 import com.henrique.aplicacao.services.exceptions.DataIntegrityException;
 import com.henrique.aplicacao.services.exceptions.ObjectNotFoundException;
@@ -44,6 +46,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos cadastrados");
 		};
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 
 }
